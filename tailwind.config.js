42159db3c070b9479a2690c580f6360003e57ca8
NoirHive <saimunsajal12@gmail.com>
2025-12-14 17:52:49 +0600

    Delete tailwind.config.js

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-
-module.exports = {
-  darkMode: 'class',
-  content: ['./app/**/*.{js,jsx}', './components/**/*.{js,jsx}'],
-  theme: {
-    extend: { colors: { brand: '#f97316' } }
-  },
-  plugins: []
-}
