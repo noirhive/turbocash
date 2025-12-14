a443c2293f066e602abe43deaff64ea4f5669990
NoirHive <saimunsajal12@gmail.com>
2025-12-14 16:42:55 +0600

    Delete app directory

--- app/dashboard/page.js
+++
@@ -1,8 +0,0 @@
-
-import { supabase } from '../../lib/supabase'
-import MonthlyTable from '../../components/MonthlyTable'
-
-export default async function Dashboard() {
-  const { data } = await supabase.from('transactions').select('*').order('date')
-  return <MonthlyTable data={data || []} />
-}
--- app/page.js
+++
@@ -1,6 +0,0 @@
-
-import TransactionForm from '../components/TransactionForm'
-
-export default function Home() {
-  return <TransactionForm />
-}
