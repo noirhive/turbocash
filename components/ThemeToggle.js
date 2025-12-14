4479a7120a758825d4b4fcbb19715df6834f870d
NoirHive <saimunsajal12@gmail.com>
2025-12-14 16:43:04 +0600

    Delete components directory

--- components/ThemeToggle.js
+++
@@ -1,8 +0,0 @@
-
-'use client'
-import { useTheme } from 'next-themes'
-
-export default function ThemeToggle() {
-  const { theme, setTheme } = useTheme()
-  return <button onClick={()=>setTheme(theme==='dark'?'light':'dark')}>Toggle</button>
-}
